a2bec52190f3300a9974e524234397bfbb17a034
Yenifer01 <yenifer.laiza@tecsup.edu.pe>
2024-06-22 01:51:55 -0500

    testimonios

--- src/app/components/testimonios/testimonios.component.ts
+++ src/app/components/testimonios/testimonios.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+interface Testimonial {
+  img: string;
+  name: string;
+  position: string;
+  text: string;
+}
 @Component({
   selector: 'app-testimonios',
   standalone: true,
@@ -11,6 +16,46 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './testimonios.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TestimoniosComponent { 
+export class TestimoniosComponent implements OnInit { 
+
+   testimonials: Testimonial[] = [
+    {
+      img: 'assets/img/testimonial-1.jpg',
+      name: 'Juanito Perez',
+      position: 'Maestro',
+      text: 'Estoy realmente impresionado por el impacto positivo ...'
+    },
+    {
+      img: 'assets/img/testimonial-2.jpg',
+      name: 'Maria Sanchez',
+      position: 'Estudiante',
+      text: 'Las actividades benéficas de esta organización son realmente inspiradoras...'
+    },
+    {
+      img: 'assets/img/testimonial-3.jpg',
+      name: 'Juan Carlos Rodriguez',
+      position: 'Ingeniero',
+      text: 'Desde que me uní como voluntario, he sido testigo del impacto positivo...'
+    },
+    {
+      img: 'assets/img/testimonial-4.jpg',
+      name: 'Adriana Moreno',
+      position: 'Diseñadora',
+      text: 'Me encanta formar parte de estas actividades benéficas...'
+    }
+  ];
+
+  testimonialGroups: Testimonial[][] = [];
+
+  ngOnInit(): void {
+    this.groupTestimonials();
+  }
+
+  groupTestimonials(): void {
+    const groupSize = 3;
+    for (let i = 0; i < this.testimonials.length; i += groupSize) {
+      this.testimonialGroups.push(this.testimonials.slice(i, i + groupSize));
+    }
+  }
   
 }
