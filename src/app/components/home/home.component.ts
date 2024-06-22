f57cd5c176df26c61d8a23d9c81b38deedf0ff89
Yenifer01 <yenifer.laiza@tecsup.edu.pe>
2024-06-22 03:12:12 -0500

    blog

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,15 +3,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CarrouselComponent } from '../carrousel/carrousel.component';
 import { TestimoniosComponent } from '../testimonios/testimonios.component';
 import { InfoComponent } from '../info/info.component';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    CommonModule, CarrouselComponent, TestimoniosComponent, InfoComponent
-  ],
-  templateUrl:'./home.component.html',
-  styleUrl: './home.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule, CarrouselComponent, TestimoniosComponent, InfoComponent,
+        FooterComponent
+    ]
 })
 export class HomeComponent { }
--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InfoComponent } from '../info/info.component';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-nosotros',
-  standalone: true,
-  imports: [
-    CommonModule, InfoComponent
-  ],
-  templateUrl: './nosotros.component.html',
-  styleUrl: './nosotros.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-nosotros',
+    standalone: true,
+    templateUrl: './nosotros.component.html',
+    styleUrl: './nosotros.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule, InfoComponent,
+        FooterComponent
+    ]
 })
 export class NosotrosComponent { }
