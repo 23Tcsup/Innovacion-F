9489a56cea2c13e806556363cff4f2ec7d51868c
Yenifer01 <yenifer.laiza@tecsup.edu.pe>
2024-06-22 07:52:40 -0500

    voluntariado

--- src/app/components/voluntariado/voluntariado.component.ts
+++ src/app/components/voluntariado/voluntariado.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TestimoniosComponent } from "../testimonios/testimonios.component";
 
 @Component({
-  selector: 'app-voluntariado',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './voluntariado.component.html',
-  styleUrl: './voluntariado.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-voluntariado',
+    standalone: true,
+    templateUrl: './voluntariado.component.html',
+    styleUrl: './voluntariado.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        TestimoniosComponent
+    ]
 })
 export class VoluntariadoComponent { }
